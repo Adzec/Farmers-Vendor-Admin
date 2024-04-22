8c4ec9d3ddf4e9b665280206119d1846b0650934
Jane49-cloud <janendirangu49@gmail.com>
2024-04-22 12:05:15 +0300

    fixed a bug

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,16 @@ import Login from '../pages/Login';
 import Dashboard from '../pages/Dashboard';
 import ApprovedUsers from '../pages/Sellers/Approved';
 import { ToastContainer } from 'react-toastify';
+import { customToastContainerStyle } from '../utils/utils';
 
 const Paths = () => {
     return (
         <div>
-            <ToastContainer position="top-center" />
+            <ToastContainer
+                position="top-center"
+                toastClassName="custom-toast"
+                style={customToastContainerStyle}
+            />
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/" element={<Dashboard />} />
--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -6,3 +6,11 @@ export const getInitials = (user: any) => {
     const initials = firstLetters.slice(0, 2).join('');
     return initials.toUpperCase();
 };
+
+export const customToastContainerStyle = {
+    backgroundColor: '#333', // Background color
+    color: '#fff', // Text color
+    borderRadius: '8px', // Border radius
+    boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)', // Box shadow
+    width: '300px', // Width
+};
