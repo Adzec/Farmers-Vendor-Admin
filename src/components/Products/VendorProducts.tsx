8d43dad161a1e9f6ba02779db137bce063cb83f6
Jane49-cloud <janendirangu49@gmail.com>
2024-05-02 19:09:11 +0300

    Add Vendor Product listings

---
+++ src/components/Products/VendorProducts.tsx
@@ -0,0 +1,23 @@
+import { products } from '../../Data/Data';
+import ProductCard from '../Global/ProductCard';
+
+const ProductListing = () => {
+    return (
+        <div className="w-full lg:w-[80%]">
+            <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4 mx-auto">
+                {products.map((product) => (
+                    <div key={product?.key}>
+                        <ProductCard
+                            image={product?.image}
+                            name={product.name}
+                            description="lorem ipsum dor siutim augddj yayydi ipsum loren dore si amit"
+                            price={product?.price}
+                        />
+                    </div>
+                ))}
+            </section>
+        </div>
+    );
+};
+
+export default ProductListing;
